2515be7947c5383097ef40c2e11527e4fc635070
gor1sher <164029653+gor1sher@users.noreply.github.com>
2024-06-30 23:38:25 +0300

    Добавлен новый класс HttpTaskServer

--- src/service/httpTaskManagers/HttpTaskServer.java
+++ src/service/httpTaskManagers/HttpTaskServer.java
@@ -1,6 +1,5 @@
 package service.httpTaskManagers;
 
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
